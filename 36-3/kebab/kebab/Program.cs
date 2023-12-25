f1e236a4698eee3fa2874236dc9d755a24c2729a
LahevOdVika <michalBudai@outlook.cz>
2023-12-25 17:17:04 +0100

    36-3-1-Kebab

---
+++ 36-3/kebab/kebab/Program.cs
@@ -0,0 +1,22 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+string[] radek = sr.ReadLine().Split(" ");
+int pocetObchodu = Convert.ToInt32(radek[0]);
+int cenaKm = Convert.ToInt32(radek[1]);
+int nejmensiCena = int.MaxValue;
+
+for (int i = 0; i < pocetObchodu; i++)
+{
+    string[] obchod = sr.ReadLine().Split(" ");
+    int cenaKebab = Convert.ToInt32(obchod[0]);
+    int cenaVzdalenost = Convert.ToInt32(obchod[1]) * cenaKm;
+
+    int cenaCelkem = cenaKebab + cenaVzdalenost;
+    
+    if (cenaCelkem < nejmensiCena)
+    {
+        nejmensiCena = cenaCelkem;
+    }
+}
+sw.WriteLine();
